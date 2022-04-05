e2ebd35fab66590d7c96dc562d00b427ef2be931
lailyrhmh <91611703+lailyrhmh@users.noreply.github.com>
2022-04-05 23:48:34 +0800

    Delete Transportation directory

--- Transportation/app/src/main/java/com/example/transportation/model/Transportation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.transportation.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Transportation(
-    @StringRes val carName: Int,
-    @DrawableRes val carImg: Int
-    )
